6b70aa4a419b6ff534e966b66da9774a31c197e4
Swetha-Hariharan1810 <78995069+Swetha-Hariharan1810@users.noreply.github.com>
2024-07-26 06:45:33 +0530

    Create Worklet-processor.js

---
+++ Worklet-processor.js
@@ -0,0 +1,12 @@
+// worklet-processor.js
+class PCMProcessor extends AudioWorkletProcessor {
+    process(inputs, outputs, parameters) {
+        const input = inputs[0];
+        if (input.length > 0) {
+            this.port.postMessage(input[0]);
+        }
+        return true;
+    }
+}
+
+registerProcessor('pcm-processor', PCMProcessor);
